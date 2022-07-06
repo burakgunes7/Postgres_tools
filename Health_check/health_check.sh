82812bda3cdf9c33bf4c605d8be59426f37ade2e
Burak Gunes <burakemrullahgunes@gmail.com>
2022-07-06 13:58:27 +0300

    update

--- Health_check/health_check.sh
+++ Health_check/health_check.sh
@@ -64,36 +64,46 @@ if ([ $IS_NODE1_DOWN == 1 ] && [ $IS_NODE2_DOWN == 0 ]) || ([ "$NODE1_SERVICE_ST
 fi
 
 if [ $node2_master -eq 1 ] && [ $IS_NODE1_DOWN == 0 ]; then
-    start_postgres=$(ssh root@192.168.218.135 sudo systemctl start postgresql@12-main)
-    if [ $? -eq 1 ]; then
-        echo "[ $date ] POSTGRES SERVICE START OK"
-    else
-        echo "[ $date ] POSTGRES SERVICE START FAIL"
-    fi
-    drop_replica_slot=$(psql -U postgres -h 192.168.218.135 -c "select pg_drop_replication_slot('slaveslot1');")
-    if [ $? -eq 1 ]; then
-        echo "[ $date ] POSTGRES DROP REPLICASLOT OK"
-    else
-        echo "[ $date ] POSTGRES DROP REPLICASLOT FAIL"
-    fi
+    # start_postgres=$(ssh root@192.168.218.135 sudo systemctl start postgresql@12-main)
+    # if [ $? -eq 1 ]; then
+    #     echo "[ $date ] POSTGRES SERVICE START OK"
+    # else
+    #     echo "[ $date ] POSTGRES SERVICE START FAIL"
+    # fi
+    # drop_replica_slot=$(psql -U postgres -h 192.168.218.135 -c "select pg_drop_replication_slot('slaveslot1');")
+    # if [ $? -eq 1 ]; then
+    #     echo "[ $date ] POSTGRES DROP REPLICASLOT OK"
+    # else
+    #     echo "[ $date ] POSTGRES DROP REPLICASLOT FAIL"
+    # fi
     remove=$(ssh root@192.168.218.135 sudo rm -rf /mnt/volume1)
     if [ $? -eq 0 ]; then
         echo "[ $date ] POSTGRES REMOVE FOLDER OK"
     else
         echo "[ $date ]  POSTGRES REMOVE FOLDER FAIL"
     fi
-    create_folder=$(ssh root@192.168.218.135 mkdir -p /mnt/volume1/postgresql/12/main/ && ssh root@192.168.218.135 chown -R postgres:postgres /mnt/)
-    if [ $? -eq 1 ]; then
+
+    create_folder=$(ssh root@192.168.218.135 mkdir -p /mnt/volume1/postgresql/12/main/)
+    if [ $? -eq 0 ]; then
         echo "[ $date ] POSTGRES CREATE FOLDER OK"
     else
         echo "[ $date ] POSTGRES CREATE FOLDER FAIL"
     fi
-    backup=$(ssh postgres@192.168.218.135 sudo -u postgres pg_basebackup -h 192.168.218.136 -D /mnt/volume1/postgresql/12/main/ -U replicator -P -v -R -X stream -C -S slaveslot1)
-    if [ $? -eq 1 ]; then
+
+    owner=$(ssh root@192.168.218.135 chown -R postgres:postgres /mnt/)
+
+    backup=$(
+        ssh postgres@192.168.218.135 pg_basebackup -h 192.168.218.136 -D /mnt/volume1/postgresql/12/main/ -U replicator -P -v -R -X stream -C -S slaveslot1
+    )
+    if [ $? -eq 0 ]; then
         echo "[ $date ] POSTGRES BACKUP SLAVESLOT1 OK"
     else
         echo "[ $date ] POSTGRES BACKUP SLAVESLOT1 FAIL"
     fi
+
+    permission=$(ssh root@192.168.218.135 sudo chmod 700 -R /mnt/volume1/postgresql/12/main/)
+
+    conf_reload=$(ssh root@192.168.218.135 pg_ctlcluster 12 main reload)
 fi
 
 # NODE 2 IS DOWN BUT NODE 1 IS UP || NODE 2 IS UP BUT SERVICE IS DOWN
@@ -103,7 +113,7 @@ if ([ $IS_NODE2_DOWN == 1 ] && [ $IS_NODE1_DOWN == 0 ]) || ([ "$NODE2_SERVICE_ST
     read promote_choice
     if [ $promote_choice == "y" ]; then
         promote_node1=$(ssh root@192.168.218.135 sudo pg_ctlcluster 12 main promote)
-        if [ $? -eq 1 ]; then
+        if [ $? -eq 0 ]; then
             echo "[ $date ] NODE1 successfully promoted to MASTER"
             node1_master=1
         else
@@ -115,34 +125,44 @@ if ([ $IS_NODE2_DOWN == 1 ] && [ $IS_NODE1_DOWN == 0 ]) || ([ "$NODE2_SERVICE_ST
 fi
 
 if [ $node1_master -eq 1 ] && [ $IS_NODE1_DOWN == 0 ]; then
-    start_postgres=$(ssh root@192.168.218.136 sudo systemctl start postgresql@12-main)
-    if [ $? -eq 1 ]; then
-        echo "[ $date ] POSTGRES SERVICE START OK"
-    else
-        echo "[ $date ] POSTGRES SERVICE START FAIL"
-    fi
-    drop_replica_slot=$(psql -U postgres -d deneme -h 192.168.218.136 -c "select pg_drop_replication_slot('slaveslot1');")
-    if [ $? -eq 1 ]; then
-        echo "[ $date ] POSTGRES DROP REPLICASLOT OK"
-    else
-        echo "[ $date ] POSTGRES DROP REPLICASLOT FAIL"
-    fi
-    remove=$(ssh root@192.168.218.136 sudo rm -rf /mnt/volume1/postgresql/12/main/)
+    # start_postgres=$(ssh root@192.168.218.135 sudo systemctl start postgresql@12-main)
+    # if [ $? -eq 1 ]; then
+    #     echo "[ $date ] POSTGRES SERVICE START OK"
+    # else
+    #     echo "[ $date ] POSTGRES SERVICE START FAIL"
+    # fi
+    # drop_replica_slot=$(psql -U postgres -h 192.168.218.135 -c "select pg_drop_replication_slot('slaveslot1');")
+    # if [ $? -eq 1 ]; then
+    #     echo "[ $date ] POSTGRES DROP REPLICASLOT OK"
+    # else
+    #     echo "[ $date ] POSTGRES DROP REPLICASLOT FAIL"
+    # fi
+    remove=$(ssh root@192.168.218.136 sudo rm -rf /mnt/volume1)
     if [ $? -eq 0 ]; then
         echo "[ $date ] POSTGRES REMOVE FOLDER OK"
     else
         echo "[ $date ]  POSTGRES REMOVE FOLDER FAIL"
     fi
-    create_folder=$(ssh root@192.168.218.136 mkdir /mnt/volume1/postgresql/12/main/ && chown -R postgres:postgres /mnt/volume1/postgresql/12/*)
-    if [ $? -eq 1 ]; then
+
+    create_folder=$(ssh root@192.168.218.136 mkdir -p /mnt/volume1/postgresql/12/main/)
+    if [ $? -eq 0 ]; then
         echo "[ $date ] POSTGRES CREATE FOLDER OK"
     else
         echo "[ $date ] POSTGRES CREATE FOLDER FAIL"
     fi
-    backup=$(ssh postgres@192.168.218.136 sudo -u postgres pg_basebackup -h 192.168.218.135 -D /mnt/volume1/postgresql/12/main/ -U replicator -P -v -R -X stream -C -S slaveslot1)
-    if [ $? -eq 1 ]; then
+
+    owner=$(ssh root@192.168.218.136 chown -R postgres:postgres /mnt/)
+
+    backup=$(
+        ssh postgres@192.168.218.136 pg_basebackup -h 192.168.218.135 -D /mnt/volume1/postgresql/12/main/ -U replicator -P -v -R -X stream -C -S slaveslot1
+    )
+    if [ $? -eq 0 ]; then
         echo "[ $date ] POSTGRES BACKUP SLAVESLOT1 OK"
     else
         echo "[ $date ] POSTGRES BACKUP SLAVESLOT1 FAIL"
     fi
+
+    permission=$(ssh root@192.168.218.136 sudo chmod 700 -R /mnt/volume1/postgresql/12/main/)
+
+    conf_reload=$(ssh root@192.168.218.136 pg_ctlcluster 12 main reload)
 fi
