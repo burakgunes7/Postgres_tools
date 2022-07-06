86e982fdf8ef6da59f881ec9e6627715f86612cb
Burak Gunes <burakemrullahgunes@gmail.com>
2022-07-06 16:47:08 +0300

    update

--- Health_check/health_check.sh
+++ Health_check/health_check.sh
@@ -2,11 +2,11 @@
 NODE1_IP=192.168.218.135
 NODE2_IP=192.168.218.136
 
+master_cluster=null
+replica_cluster=null
+
 IS_NODE1_DOWN=0
 IS_NODE2_DOWN=0
-
-node1_master=0
-node2_master=0
 
 FAIL=0
 
@@ -15,6 +15,15 @@ date=$(date)
 kindalightblue=$(tput setaf 123)
 echo ${kindalightblue}
 
+find=$(ssh root@$NODE1_IP find /mnt/volume1/postgresql/12/main/ -name standby.signal)
+if [ "$find" != "/mnt/volume1/postgresql/12/main/standby.signal" ]; then
+    echo "[ $date ] NODE1 is MASTER CLUSTER "
+    master_cluster=$NODE1_IP
+else
+    echo "[ $date ] NODE1 is REPLICA CLUSTER"
+    replica_cluster=$NODE1_IP
+fi
+
 ping -c 3 $NODE1_IP >/dev/null 2>&1
 if [ $? -ne 0 ]; then
     IS_NODE1_DOWN=1
@@ -22,12 +31,21 @@ if [ $? -ne 0 ]; then
 else
     echo "[ $date ] NODE1 POSTGRES SERVER is UP"
 
-    NODE1_SERVICE_STAT=$(ssh root@192.168.218.135 systemctl is-active postgresql@12-main)
+    NODE1_SERVICE_STAT=$(ssh root@$NODE1_IP systemctl is-active postgresql@12-main)
     if [ "$NODE1_SERVICE_STAT" == "active" ]; then
         echo "[ $date ] NODE1 POSTGRES SERVICE is UP"
     else
         echo "[ $date ] NODE1 POSTGRES SERVICE is DOWN"
     fi
+fi
+
+find=$(ssh root@$NODE2_IP find /mnt/volume1/postgresql/12/main/ -name standby.signal)
+if [ "$find" != "/mnt/volume1/postgresql/12/main/standby.signal" ]; then
+    echo "[ $date ] NODE2 is MASTER CLUSTER "
+    master_cluster=$NODE2_IP
+else
+    echo "[ $date ] NODE2 is REPLICA CLUSTER"
+    replica_cluster=$NODE2_IP
 fi
 
 ping -c 3 $NODE2_IP >/dev/null 2>&1
@@ -37,7 +55,7 @@ if [ $? -ne 0 ]; then
 else
     echo "[ $date ] NODE2 POSTGRES SERVER is UP"
 
-    NODE2_SERVICE_STAT=$(ssh root@192.168.218.136 systemctl is-active postgresql@12-main)
+    NODE2_SERVICE_STAT=$(ssh root@$NODE2_IP systemctl is-active postgresql@12-main)
     if [ "$NODE2_SERVICE_STAT" == "active" ]; then
         echo "[ $date ] NODE2 POSTGRES SERVICE is UP"
     else
@@ -45,124 +63,18 @@ else
     fi
 fi
 
-# NODE 1 IS DOWN BUT NODE 2 IS UP || NODE 1 IS UP BUT SERVICE IS DOWN
-#
-if ([ $IS_NODE1_DOWN == 1 ] && [ $IS_NODE2_DOWN == 0 ]) || ([ "$NODE1_SERVICE_STAT" != "active" ] && [ $IS_NODE2_DOWN == 0 ]); then
-    echo "Do you want to promote NODE2 to MASTER? (y-n)"
-    read promote_choice
-    if [ $promote_choice == "y" ]; then
-        promote_node2=$(ssh root@192.168.218.136 sudo pg_ctlcluster 12 main promote)
-        if [ $? -eq 0 ]; then
-            echo "[ $date ] NODE2 successfully promoted to MASTER"
-            node2_master=1
-        else
-            echo "[ $date ] NODE2 failed to be promoted to MASTER"
-        fi
-    else
-        echo "Bye"
-    fi
-fi
+if [ "$NODE1_IP" = "$master_cluster" ] && ([ $IS_NODE1_DOWN -eq 1 ] || [ "$NODE1_SERVICE_STAT" != "active" ]); then
+    echo "NODE 1 IS MASTER AND DOWN"
+    export master_cluster
+    export replica_cluster
+    export IS_NODE1_DOWN
+    /home/turkai/Desktop/Postgres/Health_check/promote_master.sh
 
-if [ $node2_master -eq 1 ] && [ $IS_NODE1_DOWN == 0 ]; then
-    # start_postgres=$(ssh root@192.168.218.135 sudo systemctl start postgresql@12-main)
-    # if [ $? -eq 1 ]; then
-    #     echo "[ $date ] POSTGRES SERVICE START OK"
-    # else
-    #     echo "[ $date ] POSTGRES SERVICE START FAIL"
-    # fi
-    # drop_replica_slot=$(psql -U postgres -h 192.168.218.135 -c "select pg_drop_replication_slot('slaveslot1');")
-    # if [ $? -eq 1 ]; then
-    #     echo "[ $date ] POSTGRES DROP REPLICASLOT OK"
-    # else
-    #     echo "[ $date ] POSTGRES DROP REPLICASLOT FAIL"
-    # fi
-    remove=$(ssh root@192.168.218.135 sudo rm -rf /mnt/volume1)
-    if [ $? -eq 0 ]; then
-        echo "[ $date ] POSTGRES REMOVE FOLDER OK"
-    else
-        echo "[ $date ]  POSTGRES REMOVE FOLDER FAIL"
-    fi
+elif [ "$NODE2_IP" = "$master_cluster" ] && ([ $IS_NODE2_DOWN -eq 1 ] || [ "$NODE2_SERVICE_STAT" != "active" ]); then
+    echo "NODE 2 IS MASTER AND DOWN"
+    export master_cluster
+    export replica_cluster
+    export IS_NODE2_DOWN
+    /home/turkai/Desktop/Postgres/Health_check/promote_master.sh
 
-    create_folder=$(ssh root@192.168.218.135 mkdir -p /mnt/volume1/postgresql/12/main/)
-    if [ $? -eq 0 ]; then
-        echo "[ $date ] POSTGRES CREATE FOLDER OK"
-    else
-        echo "[ $date ] POSTGRES CREATE FOLDER FAIL"
-    fi
-
-    owner=$(ssh root@192.168.218.135 chown -R postgres:postgres /mnt/)
-
-    backup=$(
-        ssh postgres@192.168.218.135 pg_basebackup -h 192.168.218.136 -D /mnt/volume1/postgresql/12/main/ -U replicator -P -v -R -X stream -C -S slaveslot1
-    )
-    if [ $? -eq 0 ]; then
-        echo "[ $date ] POSTGRES BACKUP SLAVESLOT1 OK"
-    else
-        echo "[ $date ] POSTGRES BACKUP SLAVESLOT1 FAIL"
-    fi
-
-    permission=$(ssh root@192.168.218.135 sudo chmod 700 -R /mnt/volume1/postgresql/12/main/)
-
-    conf_reload=$(ssh root@192.168.218.135 pg_ctlcluster 12 main reload)
-fi
-
-# NODE 2 IS DOWN BUT NODE 1 IS UP || NODE 2 IS UP BUT SERVICE IS DOWN
-#
-if ([ $IS_NODE2_DOWN == 1 ] && [ $IS_NODE1_DOWN == 0 ]) || ([ "$NODE2_SERVICE_STAT" != "active" ] && [ $IS_NODE1_DOWN == 0 ]); then
-    echo "Do you want to promote NODE1 to MASTER? (y-n)"
-    read promote_choice
-    if [ $promote_choice == "y" ]; then
-        promote_node1=$(ssh root@192.168.218.135 sudo pg_ctlcluster 12 main promote)
-        if [ $? -eq 0 ]; then
-            echo "[ $date ] NODE1 successfully promoted to MASTER"
-            node1_master=1
-        else
-            echo "[ $date ] NODE1 failed to be promoted to MASTER"
-        fi
-    else
-        echo "Bye"
-    fi
-fi
-
-if [ $node1_master -eq 1 ] && [ $IS_NODE1_DOWN == 0 ]; then
-    # start_postgres=$(ssh root@192.168.218.135 sudo systemctl start postgresql@12-main)
-    # if [ $? -eq 1 ]; then
-    #     echo "[ $date ] POSTGRES SERVICE START OK"
-    # else
-    #     echo "[ $date ] POSTGRES SERVICE START FAIL"
-    # fi
-    # drop_replica_slot=$(psql -U postgres -h 192.168.218.135 -c "select pg_drop_replication_slot('slaveslot1');")
-    # if [ $? -eq 1 ]; then
-    #     echo "[ $date ] POSTGRES DROP REPLICASLOT OK"
-    # else
-    #     echo "[ $date ] POSTGRES DROP REPLICASLOT FAIL"
-    # fi
-    remove=$(ssh root@192.168.218.136 sudo rm -rf /mnt/volume1)
-    if [ $? -eq 0 ]; then
-        echo "[ $date ] POSTGRES REMOVE FOLDER OK"
-    else
-        echo "[ $date ]  POSTGRES REMOVE FOLDER FAIL"
-    fi
-
-    create_folder=$(ssh root@192.168.218.136 mkdir -p /mnt/volume1/postgresql/12/main/)
-    if [ $? -eq 0 ]; then
-        echo "[ $date ] POSTGRES CREATE FOLDER OK"
-    else
-        echo "[ $date ] POSTGRES CREATE FOLDER FAIL"
-    fi
-
-    owner=$(ssh root@192.168.218.136 chown -R postgres:postgres /mnt/)
-
-    backup=$(
-        ssh postgres@192.168.218.136 pg_basebackup -h 192.168.218.135 -D /mnt/volume1/postgresql/12/main/ -U replicator -P -v -R -X stream -C -S slaveslot1
-    )
-    if [ $? -eq 0 ]; then
-        echo "[ $date ] POSTGRES BACKUP SLAVESLOT1 OK"
-    else
-        echo "[ $date ] POSTGRES BACKUP SLAVESLOT1 FAIL"
-    fi
-
-    permission=$(ssh root@192.168.218.136 sudo chmod 700 -R /mnt/volume1/postgresql/12/main/)
-
-    conf_reload=$(ssh root@192.168.218.136 pg_ctlcluster 12 main reload)
 fi
---
+++ Health_check/promote_master.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+FAIL=0
+
+date=$(date)
+
+kindalightblue=$(tput setaf 123)
+echo ${kindalightblue}
+
+# NODE 1 IS DOWN BUT NODE 2 IS UP || NODE 1 IS UP BUT SERVICE IS DOWN
+#
+promote=$(ssh root@$replica_cluster sudo pg_ctlcluster 12 main promote)
+if [ $? -eq 0 ]; then
+    echo "[ $date ] REPLICA at $replica_cluster successfully promoted to MASTER"
+    replicaTo_master=1
+else
+    echo "[ $date ] REPLICA at $replica_cluster failed to be promoted to MASTER"
+fi
+
+if [ $replicaTo_master -eq 1 ] && [ $IS_NODE1_DOWN == 0 ]; then
+    remove=$(ssh root@$master_cluster sudo rm -rf /mnt/volume1)
+    if [ $? -eq 0 ]; then
+        echo "[ $date ] POSTGRES REMOVE FOLDER OK"
+    else
+        echo "[ $date ]  POSTGRES REMOVE FOLDER FAIL"
+    fi
+
+    create_folder=$(ssh root@$master_cluster mkdir -p /mnt/volume1/postgresql/12/main/)
+    if [ $? -eq 0 ]; then
+        echo "[ $date ] POSTGRES CREATE FOLDER OK"
+    else
+        echo "[ $date ] POSTGRES CREATE FOLDER FAIL"
+    fi
+
+    owner=$(ssh root@$master_cluster chown -R postgres:postgres /mnt/)
+
+    backup=$(
+        ssh postgres@$master_cluster pg_basebackup -h $replica_cluster -D /mnt/volume1/postgresql/12/main/ -U replicator -P -v -R -X stream -C -S slaveslot1
+    )
+    if [ $? -eq 0 ]; then
+        echo "[ $date ] POSTGRES BACKUP SLAVESLOT1 OK"
+    else
+        echo "[ $date ] POSTGRES BACKUP SLAVESLOT1 FAIL"
+    fi
+
+    permission=$(ssh root@$master_cluster sudo chmod 700 -R /mnt/volume1/postgresql/12/main/)
+
+    conf_reload=$(ssh root@$master_cluster pg_ctlcluster 12 main reload)
+fi
