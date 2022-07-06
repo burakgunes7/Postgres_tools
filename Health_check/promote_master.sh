14059d56dde7f305953b707484da3a52fdef0801
Burak Gunes <burakemrullahgunes@gmail.com>
2022-07-06 17:01:47 +0300

    update

--- Health_check/promote_master.sh
+++ Health_check/promote_master.sh
@@ -18,29 +18,36 @@ else
 fi
 
 if [ $replicaTo_master -eq 1 ]; then
-    remove=$(ssh root@$master_cluster sudo rm -rf /mnt/volume1)
+    remove_main_folder=$(ssh root@$master_cluster sudo rm -rf /mnt/volume1)
     if [ $? -eq 0 ]; then
         echo "[ $date ] POSTGRES REMOVE FOLDER OK"
     else
         echo "[ $date ]  POSTGRES REMOVE FOLDER FAIL"
     fi
 
-    create_folder=$(ssh root@$master_cluster mkdir -p /mnt/volume1/postgresql/12/main/)
+    create_main_folder=$(ssh root@$master_cluster mkdir -p /mnt/volume1/postgresql/12/main/)
     if [ $? -eq 0 ]; then
         echo "[ $date ] POSTGRES CREATE FOLDER OK"
     else
         echo "[ $date ] POSTGRES CREATE FOLDER FAIL"
     fi
 
-    owner=$(ssh root@$master_cluster chown -R postgres:postgres /mnt/)
+    ownership=$(ssh root@$master_cluster chown -R postgres:postgres /mnt/)
 
-    backup=$(
+    base_backup=$(
         ssh postgres@$master_cluster pg_basebackup -h $replica_cluster -D /mnt/volume1/postgresql/12/main/ -U replicator -P -v -R -X stream -C -S slaveslot1
     )
     if [ $? -eq 0 ]; then
         echo "[ $date ] POSTGRES BACKUP SLAVESLOT1 OK"
     else
         echo "[ $date ] POSTGRES BACKUP SLAVESLOT1 FAIL"
+    fi
+
+    start_service=$(ssh root@$master_cluster systemctl start postgresql@12-main)
+    if [ $? -eq 0 ]; then
+        echo "[ $date ] POSTGRES SERVICE START OK"
+    else
+        echo "[ $date ] POSTGRES SERVICE START FAIL"
     fi
 
     permission=$(ssh root@$master_cluster sudo chmod 700 -R /mnt/volume1/postgresql/12/main/)
