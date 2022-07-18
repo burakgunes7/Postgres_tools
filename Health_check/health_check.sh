1d2b9b0a154fd740786f5406d652c57b161ee9cd
Burak Gunes <burakemrullahgunes@gmail.com>
2022-07-18 16:47:26 +0300

    update

--- Health_check/health_check.sh
+++ Health_check/health_check.sh
@@ -1,86 +1,101 @@
 #!/bin/bash
-export $(grep -v '^#' .env | xargs)
+health_check() {
 
-master_cluster=null
-replica_cluster=null
+    export $(grep -v '^#' .env | xargs)
 
-IS_NODE1_DOWN=0
-IS_NODE2_DOWN=0
+    master_cluster=null
+    replica_cluster=null
 
-FAIL=0
+    IS_NODE1_DOWN=0
+    IS_NODE2_DOWN=0
 
-date=$(date)
+    FAIL=0
 
-kindalightblue=$(tput setaf 123)
-echo ${kindalightblue}
+    date=$(date)
 
-ping -c 3 $NODE1_IP >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-    IS_NODE1_DOWN=1
-    echo "[ $date ]  NODE1 POSTGRES SERVER is DOWN"
-else
-    echo "[ $date ] NODE1 POSTGRES SERVER is UP"
+    kindalightblue=$(tput setaf 123)
+    echo ${kindalightblue}
 
-    NODE1_SERVICE_STAT=$(ssh root@$NODE1_IP systemctl is-active postgresql@12-main)
-    if [ "$NODE1_SERVICE_STAT" == "active" ]; then
-        echo "[ $date ] NODE1 POSTGRES SERVICE is UP"
+    # PING NODE_1 TO CHECK IF SERVER IS UP
+    ping -c 3 $NODE1_IP >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        IS_NODE1_DOWN=1
+        echo "[ $date ]  NODE1 POSTGRES SERVER is DOWN"
     else
-        echo "[ $date ] NODE1 POSTGRES SERVICE is DOWN"
-    fi
-fi
+        echo "[ $date ] NODE1 POSTGRES SERVER is UP"
 
-if [ $IS_NODE1_DOWN -ne 1 ]; then
-    find=$(ssh root@$NODE1_IP find /mnt/volume1/postgresql/12/main/ -name standby.signal)
-    if [ "$find" != "/mnt/volume1/postgresql/12/main/standby.signal" ]; then
-        echo "[ $date ] NODE1 is MASTER CLUSTER "
-        master_cluster=$NODE1_IP
+        #   CHECK THE POSTGRES SERVER IF THE SERVER IS UP
+        NODE1_SERVICE_STAT=$(ssh root@$NODE1_IP systemctl is-active postgresql@12-main)
+        if [ "$NODE1_SERVICE_STAT" == "active" ]; then
+            echo "[ $date ] NODE1 POSTGRES SERVICE is UP"
+        else
+            echo "[ $date ] NODE1 POSTGRES SERVICE is DOWN"
+        fi
+    fi
+
+    # CHECK IF THE SERVER IS MASTER NODE OR REPLICA NODE IF NODE_1 IS UP
+    if [ $IS_NODE1_DOWN -ne 1 ]; then
+        find=$(ssh root@$NODE1_IP find /mnt/volume1/postgresql/12/main/ -name standby.signal)
+        if [ "$find" != "/mnt/volume1/postgresql/12/main/standby.signal" ]; then
+            echo "[ $date ] NODE1 is MASTER CLUSTER "
+            master_cluster=$NODE1_IP
+        else
+            echo "[ $date ] NODE1 is REPLICA CLUSTER"
+            replica_cluster=$NODE1_IP
+        fi
+    fi
+
+    # PING NODE_2 TO CHECK IF SERVER IS UP
+    ping -c 3 $NODE2_IP >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        IS_NODE2_DOWN=1
+        echo "[ $date ] NODE2 POSTGRES SERVER is DOWN"
     else
-        echo "[ $date ] NODE1 is REPLICA CLUSTER"
-        replica_cluster=$NODE1_IP
+        echo "[ $date ] NODE2 POSTGRES SERVER is UP"
+
+        #   CHECK THE POSTGRES SERVER IF THE SERVER IS UP
+        NODE2_SERVICE_STAT=$(ssh root@$NODE2_IP systemctl is-active postgresql@12-main)
+        if [ "$NODE2_SERVICE_STAT" == "active" ]; then
+            echo "[ $date ] NODE2 POSTGRES SERVICE is UP"
+        else
+            echo "[ $date ] NODE2 POSTGRES SERVICE is DOWN"
+        fi
     fi
-fi
 
-ping -c 3 $NODE2_IP >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-    IS_NODE2_DOWN=1
-    echo "[ $date ] NODE2 POSTGRES SERVER is DOWN"
-else
-    echo "[ $date ] NODE2 POSTGRES SERVER is UP"
+    # CHECK IF THE SERVER IS MASTER NODE OR REPLICA NODE IF NODE_2 IS UP
+    if [ $IS_NODE2_DOWN -ne 1 ]; then
 
-    NODE2_SERVICE_STAT=$(ssh root@$NODE2_IP systemctl is-active postgresql@12-main)
-    if [ "$NODE2_SERVICE_STAT" == "active" ]; then
-        echo "[ $date ] NODE2 POSTGRES SERVICE is UP"
-    else
-        echo "[ $date ] NODE2 POSTGRES SERVICE is DOWN"
+        find=$(ssh root@$NODE2_IP find /mnt/volume1/postgresql/12/main/ -name standby.signal)
+        if [ "$find" != "/mnt/volume1/postgresql/12/main/standby.signal" ]; then
+            echo "[ $date ] NODE2 is MASTER CLUSTER "
+            master_cluster=$NODE2_IP
+        else
+            echo "[ $date ] NODE2 is REPLICA CLUSTER"
+            replica_cluster=$NODE2_IP
+        fi
     fi
-fi
-
-if [ $IS_NODE2_DOWN -ne 1 ]; then
-
-    find=$(ssh root@$NODE2_IP find /mnt/volume1/postgresql/12/main/ -name standby.signal)
-    if [ "$find" != "/mnt/volume1/postgresql/12/main/standby.signal" ] && [ IS_NODE2_DOWN -ne 1 ]; then
-        echo "[ $date ] NODE2 is MASTER CLUSTER "
-        master_cluster=$NODE2_IP
-    else
-        echo "[ $date ] NODE2 is REPLICA CLUSTER"
-        replica_cluster=$NODE2_IP
-    fi
-fi
-
-if [ "$NODE1_IP" = "$master_cluster" ] && ([ $IS_NODE1_DOWN -eq 1 ] || [ "$NODE1_SERVICE_STAT" != "active" ]); then
-    echo "NODE1 IS MASTER AND DOWN"
-    echo "Commencing Promote of NODE2..."
 
     export master_cluster
+    export IS_NODE1_DOWN
+    export NODE1_SERVICE_STAT
     export replica_cluster
-    /home/turkai/Desktop/Postgres/Health_check/promote_master.sh
-elif
-    [ "$NODE2_IP" = "$master_cluster" ] && ([ $IS_NODE2_DOWN -eq 1 ] || [ "$NODE2_SERVICE_STAT" != "active" ])
-then
-    echo "NODE2 IS MASTER AND DOWN"
-    echo "Commencing Promote of NODE1..."
+    export IS_NODE2_DOWN
+    export NODE2_SERVICE_STAT
+}
+# if [ "$NODE1_IP" = "$master_cluster" ] && ([ $IS_NODE1_DOWN -eq 1 ] || [ "$NODE1_SERVICE_STAT" != "active" ]); then
+#     echo "NODE1 IS MASTER AND DOWN"
+#     echo "Commencing Promote of NODE2..."
 
-    export master_cluster
-    export replica_cluster
-    /home/turkai/Desktop/Postgres/Health_check/promote_master.sh
-fi
+#     export master_cluster
+#     export replica_cluster
+#     /home/turkai/Desktop/Postgres/Health_check/promote_master.sh
+# elif
+#     [ "$NODE2_IP" = "$master_cluster" ] && ([ $IS_NODE2_DOWN -eq 1 ] || [ "$NODE2_SERVICE_STAT" != "active" ])
+# then
+#     echo "NODE2 IS MASTER AND DOWN"
+#     echo "Commencing Promote of NODE1..."
+
+#     export master_cluster
+#     export replica_cluster
+#     /home/turkai/Desktop/Postgres/Health_check/promote_master.sh
+# fi
--- Health_check/main.sh
+++ Health_check/main.sh
@@ -6,5 +6,27 @@ echo -e "\nNode IP's :"
 echo $NODE1_IP
 echo $NODE2_IP
 
+# HEALTH CHECK STARTS
 echo -e "\nHealth Check Starting...\n"
-/home/turkai/Desktop/Postgres/Health_check/health_check.sh
+
+. /home/turkai/Desktop/Postgres/Health_check/health_check.sh
+
+health_check
+
+if [ "$NODE1_IP" = "$master_cluster" ] && ([ $IS_NODE1_DOWN -eq 1 ] || [ "$NODE1_SERVICE_STAT" != "active" ]); then
+    echo "NODE1 IS MASTER AND DOWN"
+    echo "Commencing Promote of NODE2..."
+
+    export master_cluster
+    export replica_cluster
+    # /home/turkai/Desktop/Postgres/Health_check/promote_master.sh
+elif
+    [ "$NODE2_IP" = "$master_cluster" ] && ([ $IS_NODE2_DOWN -eq 1 ] || [ "$NODE2_SERVICE_STAT" != "active" ])
+then
+    echo "NODE2 IS MASTER AND DOWN"
+    echo "Commencing Promote of NODE1..."
+
+    export master_cluster
+    export replica_cluster
+    # /home/turkai/Desktop/Postgres/Health_check/promote_master.sh
+fi
